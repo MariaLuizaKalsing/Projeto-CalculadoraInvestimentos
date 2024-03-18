413e723efec0202f390a640a95392c059e613b1b
Maria Luiza Kalsing <luizak2010@hotmail.com>
2024-03-18 17:04:28 -0300

    Formulário dos campos solicitados na tela, completo

--- main.js
+++ main.js
@@ -2,21 +2,29 @@ import { generateReturnsArray } from "./src/investimentsGoals";
 
 const form = document.getElementById("investment-form");
 //const calculateButton = document.getElementById("calculate-results");
+const clearFormButton = document.getElementById("clear-form");
 
 function renderProgression(evt) {
   evt.preventDefault();
+  if (document.querySelector(".error")) {
+    return;
+  }
   // const startingAmount = number(form["starting-amount"].value);
   const startingAmount = Number(
-    document.getElementById("starting-amount").value
+    document.getElementById("starting-amount").value.replace(",", ".")
   );
   const additionalContribution = Number(
-    document.getElementById("additional-contribution").value
+    document.getElementById("additional-contribution").value.replace(",", ".")
   );
   const timeAmount = Number(document.getElementById("time-amount").value);
   const timeAmountPeriod = document.getElementById("time-amount-period").value;
-  const returnRate = Number(document.getElementById("return-rate").value);
+  const returnRate = Number(
+    document.getElementById("return-rate").value.replace(",", ".")
+  );
   const returnRatePeriod = document.getElementById("evaluation-period").value;
-  const taxRate = Number(document.getElementById("tax-rate").value);
+  const taxRate = Number(
+    document.getElementById("tax-rate").value.replace(",", ".")
+  );
 
   const returnsArray = generateReturnsArray(
     startingAmount,
@@ -29,5 +37,57 @@ function renderProgression(evt) {
   console.log(returnsArray);
 }
 
-//form.addEventListener("submit", renderProgression);
-calculateButton.addEventListener("click", renderProgression);
+function clearForm() {
+  form["starting-amount"].value = "";
+  form["additional-contribution"].value = "";
+  form["time-amount"].value = "";
+  form["return-rate"].value = "";
+  form["tax-rate"].value = "";
+
+  const errorInputontainers = document.querySelectorAll(".error");
+
+  for (const errorInputContainer of errorInputontainers) {
+    errorInputContainer.classList.remove("error");
+    errorInputContainer.parentElement.querySelector("p").remove();
+  }
+}
+function validateInput(evt) {
+  //console.log(evt.target); --> comando para verificar onde o cursor foi clicado
+  if (evt.target.value === "") {
+    return;
+  }
+
+  const { parentElement } = evt.target;
+  const grandParentElement = evt.target.parentElement.parentElement;
+  const inputValue = evt.target.value.replace(",", ".");
+
+  if (
+    !parentElement.classList.contains("error") &&
+    (isNaN(inputValue) || Number(inputValue) <= 0)
+  ) {
+    //objetivo:  <p class="text-red-500 font-bold">Insira um valor numérico e maior que zeros!</p>
+    const errorTextElement = document.createElement("p"); // cria esta tag <p></>
+    errorTextElement.classList.add("text-red-500"); // <p class="text-red-500"></p>
+    errorTextElement.classList.add("font-bold"); // <p class="text-red-500 font-bold"></p>
+    errorTextElement.innerText = "Insira um valor numérico e maior que zeros!"; //  <p class="text-red-500 font-bold">Insira um valor numérico e maior que zeros!</p>
+
+    parentElement.classList.add("error");
+    grandParentElement.appendChild(errorTextElement);
+  } else if (
+    parentElement.classList.contains("error") &&
+    !isNaN(inputValue) &&
+    Number(inputValue) > 0
+  ) {
+    parentElement.classList.remove("error");
+    grandParentElement.querySelector("p").remove();
+  }
+}
+for (const formElement of form) {
+  if (formElement.tagName === "INPUT" && formElement.hasAttribute("name")) {
+    formElement.addEventListener("blur", validateInput);
+  }
+}
+
+form.addEventListener("submit", renderProgression);
+//calculateButton.addEventListener("click", renderProgression);
+clearFormButton.addEventListener("click", clearForm);
